d55edc9263b87b28f27ff09890a40ee8add6e216
seagiv <schayka@gmail.com>
2020-01-27 12:48:23 +0200

    optimized Vulkan benchmarks

--- test/test.cpp
+++ test/test.cpp
@@ -6,7 +6,11 @@
 #include <conio.h>
 #endif
 
-#define	LABELS_COUNT	25000
+#ifndef min
+# define min(a, b)  ((a) < (b) ? (a) : (b))
+#endif
+
+#define	LABELS_COUNT	250000
 
 static void print(uint8_t *data)
 {
@@ -20,7 +24,7 @@ int main()
 {
 	uint8_t id[32] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
 	uint8_t salt[32] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	uint8_t *out[3];
+	uint8_t *out[4];
 
 	out[0] = (uint8_t *)malloc(LABELS_COUNT);
 	memset(out[0], 0, LABELS_COUNT);
@@ -31,9 +35,13 @@ int main()
 	out[2] = (uint8_t *)malloc(LABELS_COUNT);
 	memset(out[2], 0, LABELS_COUNT);
 
+	out[3] = (uint8_t *)malloc(LABELS_COUNT);
+	memset(out[3], 0, LABELS_COUNT);
+
+#if LABELS_COUNT <= 250000
 	scryptPositions(id, 0, LABELS_COUNT - 1, 8, salt, SPACEMESH_API_CPU, out[0], 512, 1, 1);
+#endif
 	scryptPositions(id, 0, LABELS_COUNT - 1, 8, salt, SPACEMESH_API_CUDA/*   | SPACEMESH_API_THROTTLED_MODE*/, out[1], 512, 1, 1);
-	scryptPositions(id, 0, LABELS_COUNT - 1, 8, salt, SPACEMESH_API_VULKAN/* | SPACEMESH_API_THROTTLED_MODE*/, out[2], 512, 1, 1);
 	scryptPositions(id, 0, LABELS_COUNT - 1, 8, salt, SPACEMESH_API_OPENCL/* | SPACEMESH_API_THROTTLED_MODE*/, out[2], 512, 1, 1);
 
 	print(out[0]);
@@ -45,32 +53,40 @@ int main()
 	}
 
 	if (0 != memcmp(out[0], out[2], LABELS_COUNT)) {
-		printf("WRONG result from OpenCL:\n");
+		printf("WRONG result from OpenCL\n");
 	}
 
-	if (int cookie = spacemesh_api_lock_gpu(SPACEMESH_API_CUDA)) {
-		for (int i = 0; i < 5; i++) {
-			scryptPositions(id, 0, LABELS_COUNT - 1, 8, salt, cookie, out[1], 512, 1, 1);
-			if (0 != memcmp(out[0], out[1], LABELS_COUNT)) {
-				printf("WRONG result from CUDA\n");
-			}
+	int cookie1 = spacemesh_api_lock_gpu(SPACEMESH_API_VULKAN);
+	int cookie2 = spacemesh_api_lock_gpu(SPACEMESH_API_VULKAN);
+
+	if (cookie1) {
+		scryptPositions(id, 0, LABELS_COUNT - 1, 8, salt, cookie1, out[2], 512, 1, 1);
+		print(out[2]);
+		if (0 != memcmp(out[1], out[2], LABELS_COUNT)) {
+			printf("WRONG result from NVIDIA Vulkan\n");
 		}
-		spacemesh_api_unlock_gpu(cookie);
 	}
 
-	if (int cookie = spacemesh_api_lock_gpu(SPACEMESH_API_OPENCL)) {
-		for (int i = 0; i < 5; i++) {
-			scryptPositions(id, 0, LABELS_COUNT - 1, 8, salt, cookie, out[2], 512, 1, 1);
-			if (0 != memcmp(out[0], out[2], LABELS_COUNT)) {
-				printf("WRONG result from OpenCL:\n");
-			}
+	if (cookie2) {
+		scryptPositions(id, 0, LABELS_COUNT - 1, 8, salt, cookie2, out[3], 512, 1, 1);
+		print(out[3]);
+		if (0 != memcmp(out[1], out[3], LABELS_COUNT)) {
+			printf("WRONG result from AMD Vulkan\n");
 		}
-		spacemesh_api_unlock_gpu(cookie);
+	}
+
+	if (cookie1) {
+		spacemesh_api_unlock_gpu(cookie1);
+	}
+
+	if (cookie2) {
+		spacemesh_api_unlock_gpu(cookie2);
 	}
 
 	free(out[0]);
 	free(out[1]);
 	free(out[2]);
+	free(out[3]);
 
 #ifdef WIN32
 	printf("\nPress any key to continue...\n");
