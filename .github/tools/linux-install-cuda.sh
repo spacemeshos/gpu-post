0cfdce32fdb0a92af6f96c90f7c2935406e0ad6d
Igor Tkachenko <ig@minerall.io>
2020-06-18 23:19:38 +0300

    Simplify tools/linux-install-cuda.sh
    Get OS version from lsb_release

--- .github/tools/linux-install-cuda.sh
+++ .github/tools/linux-install-cuda.sh
@@ -2,19 +2,15 @@
 
 set -e
 
-if [ -z "${OS}" ]; then
-    echo "OS variable empty"
-    exit 1
-fi
 if [ -z "${CUDA}" ]; then
     echo "CUDA variable empty"
     exit 1
 fi
 
-OSVERSION=$(echo $OS | sed 's/^ubuntu-//; s/\.//')
-sudo wget -qO /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${OSVERSION}/x86_64/cuda-ubuntu${OSVERSION}.pin
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${OSVERSION}/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${OSVERSION}/x86_64/ /"
+OS=$(lsb_release -r | cut -f 2 | tr -d .)
+sudo wget -qO /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${OS}/x86_64/cuda-ubuntu${OS}.pin
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${OS}/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${OS}/x86_64/ /"
 
 sudo apt update
 sudo apt install -y cuda-toolkit-${CUDA/./-}
