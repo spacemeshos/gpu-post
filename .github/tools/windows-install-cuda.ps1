a836beba8db2ef446dff8e6555184a339210639d
Igor Tkachenko <ig@minerall.io>
2020-06-20 01:13:26 +0300

    Windows debug

--- .github/tools/windows-install-cuda.ps1
+++ .github/tools/windows-install-cuda.ps1
@@ -1,2 +1,2 @@
 Invoke-WebRequest http://developer.download.nvidia.com/compute/cuda/10.2/Prod/network_installers/cuda_10.2.89_win10_network.exe -OutFile ./cuda_10.2.89_win10_network.exe
-dir
+Start-Process -Wait -FilePath ./cuda_10.2.89_win10_network.exe
