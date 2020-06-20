c2a6a46b4d41320c4884a8480cccb817534f2974
Igor Tkachenko <ig@minerall.io>
2020-06-20 15:07:15 +0300

    Windows debug

--- .github/tools/windows-install-cuda.ps1
+++
@@ -1,2 +0,0 @@
-Invoke-WebRequest http://developer.download.nvidia.com/compute/cuda/10.2/Prod/network_installers/cuda_10.2.89_win10_network.exe -OutFile ./cuda_10.2.89_win10_network.exe
-Start-Process -Wait -FilePath ./cuda_10.2.89_win10_network.exe -ArgumentList "-s nvcc,visual_studio_integration"
