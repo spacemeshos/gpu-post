0306fba73746e617d29c0b1c6be22709bf30d214
Igor Tkachenko <ig@minerall.io>
2020-06-21 23:31:53 +0300

    MacOS debug

--- .github/tools/macos-install-vulkan.sh
+++ .github/tools/macos-install-vulkan.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-url=https://sdk.lunarg.com/sdk/download/1.1.130.0/mac/vulkansdk-macos-1.1.130.0.tar.gz?Human=true
+url=https://sdk.lunarg.com/sdk/download/1.1.130.0/mac/vulkansdk-macos-1.1.130.0.tar.gz
 checksum=d6d80ab96e3b4363be969f9d256772e9cfb8f583db130076a9a9618d2551c726
 
-wget -q -O vulkansdk-macos-1.1.130.0.tar.gz $url
+wget -q -O ${url##*/} ${url}?Human=true
 shasum -c .github/tools/vulkan.sha256
 tar zxf ${url##*/}
 
