075a03cee538c44845ffa896f786a1f72d40b071
Igor Tkachenko <ig@minerall.io>
2020-10-20 17:10:49 +0300

    Bump vulkan version in test build

--- .github/tools/macos-install-vulkan.sh
+++ .github/tools/macos-install-vulkan.sh
@@ -2,18 +2,20 @@
 
 set -e
 
-url=https://sdk.lunarg.com/sdk/download/1.1.130.0/mac/vulkansdk-macos-1.1.130.0.tar.gz
-checksum=d6d80ab96e3b4363be969f9d256772e9cfb8f583db130076a9a9618d2551c726
+VERSION=1.2.141.0
+url=https://sdk.lunarg.com/sdk/download/$VERSION/mac/vulkansdk-macos-$VERSION.dmg
 
-wget -q -O ${url##*/} ${url}?Human=true
+filename=${url##*/}
+wget -q -O $filename $url
 shasum -c .github/tools/vulkan.sha256
-tar zxf ${url##*/}
+sudo hdiutil attach $filename
+cd /Volumes/vulkansdk-macos-$VERSION
+python install_vulkan.py
 
 VULKAN_ROOT_LOCATION=$PWD
-VULKAN_SDK_VERSION=1.1.130.0
-VULKAN_SDK=${VULKAN_ROOT_LOCATION}/vulkansdk-macos-${VULKAN_SDK_VERSION}/macOS
+VULKAN_SDK=${VULKAN_ROOT_LOCATION}/macOS
 echo "::set-env name=VULKAN_ROOT_LOCATION::$VULKAN_ROOT_LOCATION"
-echo "::set-env name=VULKAN_SDK_VERSION::$VULKAN_SDK_VERSION"
+echo "::set-env name=VULKAN_SDK_VERSION::$VERSION"
 echo "::set-env name=VULKAN_SDK::$VULKAN_SDK"
 echo "::set-env name=VK_ICD_FILENAMES::${VULKAN_SDK}/etc/vulkan/icd.d/MoltenVK_icd.json"
 echo "::set-env name=VK_LAYER_PATH::${VULKAN_SDK}/etc/vulkan/explicit_layers.d"
