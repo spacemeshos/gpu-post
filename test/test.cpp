42d8bcc5f89de37972e657418bb1af4334374d53
avive <aviveyal07@gmail.com>
2021-04-27 12:47:03 +0300

    print D in pow test app

--- test/test.cpp
+++ test/test.cpp
@@ -180,6 +180,14 @@ void do_test_pow(int aLabelsCount, unsigned aDiff)
 					D[i] = (1 << (8 - aDiff)) - 1;
 				}
 			}
+
+			printf("D: ");
+			for (int i = 0; i < sizeof(D); i++) {
+			     printf("%02X",(int)D[i]);
+			     i++;
+			}
+			printf("\n");
+
 			for (int i = 0; i < providersCount; i++) {
 				if (providers[i].compute_api != COMPUTE_API_CLASS_CPU)
 				{
@@ -335,7 +343,7 @@ void create_test_vector()
 					printf("Test vector: %s: %u hashes, %u h/s\n", providers[i].model, (uint32_t)hashes_computed, (uint32_t)hashes_per_sec);
 
 					const uint8_t *src = vector;
-					
+
 					for (uint32_t length = sizeof(vector); length > 0; length -= std::min<uint32_t>(32, length)) {
 						for (int i = 0; i < 32; i++) {
 							printf("0x%02x, ", *src++);
