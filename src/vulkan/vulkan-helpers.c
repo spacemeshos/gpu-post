5b900d3bf300857b959c157ea39cc24ee7800d53
Andrii Rozinko <a.rozinko@gmail.com>
2021-05-18 14:37:40 +0300

    Use env var if exists

--- src/vulkan/vulkan-helpers.c
+++ src/vulkan/vulkan-helpers.c
@@ -30,7 +30,10 @@ int initVulkanLibrary()
 			gVulkan.library = dlopen("libvulkan.so.1", RTLD_NOW | RTLD_LOCAL);
 		}
 #elif defined( __APPLE__ )
-		setenv("VK_ICD_FILENAMES", "./MoltenVK_icd.json", 1);
+		const char *idc = getenv("VK_ICD_FILENAMES");
+		if (NULL == idc || 0 == idc[0]) {
+			setenv("VK_ICD_FILENAMES", "./MoltenVK_icd.json", 1);
+		}
 		gVulkan.library = dlopen("libvulkan.dylib", RTLD_NOW | RTLD_LOCAL);
 #elif defined( _WIN32 )
 		gVulkan.library = LoadLibraryA("vulkan-1.dll");
