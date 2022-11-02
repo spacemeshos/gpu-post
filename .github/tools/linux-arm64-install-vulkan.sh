8112d0e590b6ca45c11656b5040b3e7dc885ae58
Matthias <5011972+fasmat@users.noreply.github.com>
2022-11-02 10:33:36 +0000

    Fixes the build for arm64 on linux

--- .github/tools/linux-arm64-install-vulkan.sh
+++ .github/tools/linux-arm64-install-vulkan.sh
@@ -9,7 +9,6 @@ sudo apt update
 sudo apt install libvulkan-dev
 sudo apt install glslang-dev
 sudo apt install spirv-tools
-sudo apt remove libllvm12
 
-VULKAN_VERSION=$(dpkg -s vulkan-sdk | awk '/^Version:/ {split($2, a, /\./); printf "%d.%d.%d", a[1], a[2], a[3]}')
+VULKAN_VERSION=$(dpkg -s libvulkan-dev | awk '/^Version:/ {split($2, a, /\./); printf "%d.%d.%d", a[1], a[2], a[3]}')
 echo "VULKAN_VERSION=$VULKAN_VERSION" >>$GITHUB_ENV
